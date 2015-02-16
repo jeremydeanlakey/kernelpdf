078a68193c0c3aa7db16242d5ade75e0ceb1a9f7
jeremydeanlakey <jeremy.lakey@gmail.com>
2015-02-16 15:43:24 -0700

    add commands to get source

--- kernelpdf.py
+++ kernelpdf.py
@@ -2,3 +2,7 @@
 # http://www.oldlinux.org/Linux.old/Linux-0.01/docs/Linux.pdf
 
 code_src = 'https://www.kernel.org/pub/linux/kernel/Historic/linux-0.01.tar.gz'
+
+from subprocess import call
+call(['wget', code_src])
+call(['tar', '-xzvf', 'linux-0.01.tar.gz'])
